59a759026d7ffbb8a3df125396e1ebc6e925b2d9
root <smurfka3@gmail.com>
2020-07-02 20:22:42 -0300

    second commit

--- classes/game.py
+++ classes/game.py
@@ -1,5 +1,14 @@
 import random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Person:
     def __init__(self, hp, mp, attack, defense, magic):
@@ -11,6 +20,7 @@ class Person:
         self.attack_high = attack + 10
         self.defense = defense
         self.magic = magic
+        self.actions = ["Attack", "Magic"]
 
     def generate_damage(self):
         return random.randrange(self.attack_low, self.attack_high)
@@ -43,7 +53,7 @@ class Person:
 
     def choose_action(self):
         iterator = 1
-        for item in self.actions
+        for item in self.actions:
             print(str(iterator) + ":", item)
             iterator += 1
 
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from classes.game import Person
+from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "cost": 10, "damage": 30},
          {"name": "Thunder", "cost": 10, "damage": 25},
          {"name": "Blizzard", "cost": 10, "damage": 20}]
 
 player = Person(100, 50, 30, 25, magic)
+enemy = Person(150, 60, 40, 35, magic)
 
-print("El daño del hechizo fue:", player.generate_spell_damage(1))
-print("Vida restante:", player.take_damage(10))
+running = True
+iterator = 1
+
+print(bcolors.FAIL + bcolors.BOLD + "UN ENEMIGO ESTÁ ATACANDO" + bcolors.ENDC)
+
+while running:
+    print("==================")
+    player.choose_action()
+    choice = input("Choose action: ")
+    index = int(choice) - 1
+
+    if index == 0:
+        damage = player.generate_damage()
+        enemy.take_damage(damage)
+        print("El enemigo recibio", damage, "Y su vida es:", enemy.get_hp())
+
+    enemy_damage = enemy.generate_damage()
+    player.take_damage(enemy_damage)
+    print("El enemigo infringió daño", enemy_damage, "Tu vida restante es:", player.get_hp())
+
